2eae001ca7b1e3ab2dee40d2da5e3e388121e195
Steven-Chang1114 <s1992413@ed.ac.uk>
2022-10-18 22:44:55 +0100

    Wire respack data into the demo page

--- app/src/main/java/com/specknet/pdiotapp/demo/DemoApp.kt
+++ app/src/main/java/com/specknet/pdiotapp/demo/DemoApp.kt
@@ -3,8 +3,12 @@ package com.specknet.pdiotapp.demo
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
 import android.os.Looper
+import android.util.Log
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.github.mikephil.charting.data.LineData
@@ -12,14 +16,76 @@ import com.github.mikephil.charting.data.LineDataSet
 import com.specknet.pdiotapp.R
 import com.specknet.pdiotapp.onboarding.OnBoardingActivity
 import com.specknet.pdiotapp.utils.Constants
+import com.specknet.pdiotapp.utils.RESpeckLiveData
 
 class DemoApp : AppCompatActivity() {
 
+    // global graph variables
+    lateinit var dataSet_res_accel_x: LineDataSet
+    lateinit var dataSet_res_accel_y: LineDataSet
+    lateinit var dataSet_res_accel_z: LineDataSet
+
+    var time = 0f
+    lateinit var allRespeckData: LineData
+
+    // global broadcast receiver so we can unregister it
+    lateinit var respeckLiveUpdateReceiver: BroadcastReceiver
+    lateinit var looperRespeck: Looper
+
+    val filterTestRespeck = IntentFilter(Constants.ACTION_RESPECK_LIVE_BROADCAST)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_demo)
 
+        setupPage()
+
+        // set up the broadcast receiver
+        respeckLiveUpdateReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+
+                Log.i("thread", "I am running on thread = " + Thread.currentThread().name)
+
+                val action = intent.action
+
+                if (action == Constants.ACTION_RESPECK_LIVE_BROADCAST) {
+
+                    val liveData =
+                        intent.getSerializableExtra(Constants.RESPECK_LIVE_DATA) as RESpeckLiveData
+                    Log.d("Live", "onReceive: liveData = " + liveData)
+
+                    // get all relevant intent contents
+                    val x = liveData.accelX
+                    val y = liveData.accelY
+                    val z = liveData.accelZ
+
+                    // Successfully get the data, send them to the ML model and run for update
+
+                    time += 1
+                    // updateGraph("respeck", x, y, z)
+
+                }
+            }
+        }
+
+        // register receiver on another thread
+        val handlerThreadRespeck = HandlerThread("bgThreadRespeckLive")
+        handlerThreadRespeck.start()
+        looperRespeck = handlerThreadRespeck.looper
+        val handlerRespeck = Handler(looperRespeck)
+        this.registerReceiver(respeckLiveUpdateReceiver, filterTestRespeck, null, handlerRespeck)
+
+    }
+
+    fun setupPage() {
+
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(respeckLiveUpdateReceiver)
+        looperRespeck.quit()
     }
 
 }
