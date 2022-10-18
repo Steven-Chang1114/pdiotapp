f6d9804aa684c583042fbce1dffda1aefe32c48c
Steven-Chang1114 <s1992413@ed.ac.uk>
2022-10-18 22:35:24 +0100

    wire up simple page

--- app/src/main/java/com/specknet/pdiotapp/demo/DemoApp.kt
+++ app/src/main/java/com/specknet/pdiotapp/demo/DemoApp.kt
@@ -1,4 +1,25 @@
 package com.specknet.pdiotapp.demo
 
-class DemoApp {
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.os.Looper
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.specknet.pdiotapp.R
+import com.specknet.pdiotapp.onboarding.OnBoardingActivity
+import com.specknet.pdiotapp.utils.Constants
+
+class DemoApp : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_demo)
+
+    }
+
 }
