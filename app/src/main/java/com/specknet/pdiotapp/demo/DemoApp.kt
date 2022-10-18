400475ef3d939217f0ca7587cfed10a7d744126b
Steven-Chang1114 <s1992413@ed.ac.uk>
2022-10-18 23:59:41 +0100

    wire up data

--- app/src/main/java/com/specknet/pdiotapp/demo/DemoApp.kt
+++ app/src/main/java/com/specknet/pdiotapp/demo/DemoApp.kt
@@ -15,6 +15,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import com.specknet.pdiotapp.R
+import com.specknet.pdiotapp.ml.Model
 import com.specknet.pdiotapp.onboarding.OnBoardingActivity
 import com.specknet.pdiotapp.utils.Constants
 import com.specknet.pdiotapp.utils.RESpeckLiveData
@@ -25,8 +26,10 @@ class DemoApp : AppCompatActivity() {
     lateinit var dataSet_res_accel_x: LineDataSet
     lateinit var dataSet_res_accel_y: LineDataSet
     lateinit var dataSet_res_accel_z: LineDataSet
+    lateinit var model: Model
 
     lateinit var classifiedMovement: ActionEnum
+    private lateinit var classifiedMovementField : TextView
 
     var time = 0f
     lateinit var allRespeckData: LineData
@@ -58,11 +61,23 @@ class DemoApp : AppCompatActivity() {
                     Log.d("Live", "onReceive: liveData = " + liveData)
 
                     // get all relevant intent contents
-                    val x = liveData.accelX
-                    val y = liveData.accelY
-                    val z = liveData.accelZ
+                    val accelX = liveData.accelX
+                    val accelY = liveData.accelY
+                    val accelZ = liveData.accelZ
 
-                    // Successfully get the data, send them to the ML model and run for update
+                    val x = liveData.gyro.x
+                    val y = liveData.gyro.y
+                    val z = liveData.gyro.z
+
+                    // Send data to the ML model and run for update
+                    classifyMovement(
+                        accelX,
+                        accelY,
+                        accelZ,
+                        x,
+                        y,
+                        z
+                    )
 
                     time += 1
                     // updateGraph("respeck", x, y, z)
@@ -80,15 +95,40 @@ class DemoApp : AppCompatActivity() {
 
     }
 
+    private fun classifyMovement(
+        accelX: Float,
+        accelY: Float,
+        accelZ: Float,
+        x: Float,
+        y: Float,
+        z: Float
+    ) {
+
+//        val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 50, 6), DataType.FLOAT32)
+//        inputFeature0.loadBuffer(byteBuffer)
+
+//        val output = model.process()
+
+//        Log.i("OUTPUT", "Our model predict it is ${output}")
+
+        updatePage(ActionEnum.LYING_DOWN_ON_THE_BACK.movement)
+    }
+
     private fun setupPage() {
-        var classifiedMovementField : TextView = findViewById(R.id.movement)
+        model = Model.newInstance(this)
+        classifiedMovementField = findViewById(R.id.movement)
         classifiedMovementField.text = ActionEnum.LYING_DOWN_ON_THE_BACK.movement
     }
 
+    private fun updatePage(movement: String) {
+
+        classifiedMovementField.text = movement
+    }
 
     override fun onDestroy() {
         super.onDestroy()
         unregisterReceiver(respeckLiveUpdateReceiver)
+        model.close()
         looperRespeck.quit()
     }
 
