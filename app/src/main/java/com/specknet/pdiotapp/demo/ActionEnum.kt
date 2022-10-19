c1667edd42a82318baae4ac35643944ee03ffe37
Steven-Chang1114 <s1992413@ed.ac.uk>
2022-10-19 01:20:27 +0100

    Integrate model into app

--- app/src/main/java/com/specknet/pdiotapp/demo/ActionEnum.kt
+++ app/src/main/java/com/specknet/pdiotapp/demo/ActionEnum.kt
@@ -7,12 +7,13 @@ enum class ActionEnum(val movement: String) {
     STANDING("Standing"),
     LYING_DOWN_ON_THE_LEFT_SIDE("Lying down on the left side"),
     LYING_DOWN_ON_THE_RIGHT_SIDE("Lying down on the right side"),
-    LYING_DOWN_ON_THE_FRONT("Lying down on the front"),
+    LYING_DOWN_ON_STOMACH("Lying down on stomach"),
     LYING_DOWN_ON_THE_BACK("Lying down on the back"),
-    WALKING("Walking"),
+    WALKING_AT_NORMAL_SPEED("Walking at Normal Speed"),
     RUNNING("Running or Jogging"),
     ASCENDING_STAIRS("Ascending stairs"),
     DESCENDING_STAIRS("Descending stairs"),
     DESK_WORK("Desk work"),
-    MOVEMENT("General movement")
+    MOVEMENT("General movement"),
+    LOADING("Loading")
 }
--- app/src/main/java/com/specknet/pdiotapp/demo/DemoApp.kt
+++ app/src/main/java/com/specknet/pdiotapp/demo/DemoApp.kt
@@ -10,15 +10,17 @@ import android.os.HandlerThread
 import android.os.Looper
 import android.util.Log
 import android.widget.TextView
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import com.specknet.pdiotapp.R
 import com.specknet.pdiotapp.ml.Model
-import com.specknet.pdiotapp.onboarding.OnBoardingActivity
 import com.specknet.pdiotapp.utils.Constants
 import com.specknet.pdiotapp.utils.RESpeckLiveData
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.Interpreter
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.nio.FloatBuffer
 
 class DemoApp : AppCompatActivity() {
 
@@ -26,12 +28,16 @@ class DemoApp : AppCompatActivity() {
     lateinit var dataSet_res_accel_x: LineDataSet
     lateinit var dataSet_res_accel_y: LineDataSet
     lateinit var dataSet_res_accel_z: LineDataSet
+    lateinit var data : MutableList<FloatArray>
     lateinit var model: Model
+    lateinit var floatArrayBuffer: FloatBuffer
+    lateinit var lastMovement: ActionEnum
+    lateinit var tflite : Interpreter
 
     lateinit var classifiedMovement: ActionEnum
     private lateinit var classifiedMovementField : TextView
 
-    var time = 0f
+    var counter = 0
     lateinit var allRespeckData: LineData
 
     // global broadcast receiver so we can unregister it
@@ -43,6 +49,7 @@ class DemoApp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_demo)
+        floatArrayBuffer = FloatBuffer.allocate(500)
 
         setupPage()
 
@@ -69,17 +76,13 @@ class DemoApp : AppCompatActivity() {
                     val y = liveData.gyro.y
                     val z = liveData.gyro.z
 
-                    // Send data to the ML model and run for update
-                    classifyMovement(
-                        accelX,
-                        accelY,
-                        accelZ,
-                        x,
-                        y,
-                        z
-                    )
+//                    data.add(floatArrayOf(accelX, accelY, accelZ, x, y, z))
+                    floatArrayBuffer.put(floatArrayOf(accelX, accelY, accelZ, x, y, z))
 
-                    time += 1
+                    // Send data to the ML model and run for update
+                    classifyMovement(floatArrayBuffer)
+
+                    counter += 1
                     // updateGraph("respeck", x, y, z)
 
                 }
@@ -95,34 +98,61 @@ class DemoApp : AppCompatActivity() {
 
     }
 
-    private fun classifyMovement(
-        accelX: Float,
-        accelY: Float,
-        accelZ: Float,
-        x: Float,
-        y: Float,
-        z: Float
-    ) {
+    private fun classifyMovement(floatArrayBuffer: FloatBuffer) {
+        if (counter < 50) {
+            return updatePage(lastMovement)
+        }
 
-//        val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 50, 6), DataType.FLOAT32)
-//        inputFeature0.loadBuffer(byteBuffer)
+        val inputArray = floatArrayBuffer.array().sliceArray(IntRange(0, 299))
+        val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 50, 6), DataType.FLOAT32)
+        inputFeature0.loadArray(inputArray)
 
-//        val output = model.process()
+        counter = 0
+        floatArrayBuffer.clear()
 
-//        Log.i("OUTPUT", "Our model predict it is ${output}")
+        val output = model.process(inputFeature0).outputFeature0AsTensorBuffer.floatArray
 
-        updatePage(ActionEnum.LYING_DOWN_ON_THE_BACK.movement)
+        val movementIdx = output.indexOf(output.max()!!)
+
+        Log.i("OUTPUT", "Predicted movement is ${movementIdx}")
+        val currentMovement = selectMovements(movementIdx)
+
+        lastMovement = currentMovement
+        updatePage(currentMovement)
+    }
+
+    private fun selectMovements(idx: Int) : ActionEnum {
+        return when (idx) {
+            0 -> ActionEnum.DESK_WORK
+            1 -> ActionEnum.WALKING_AT_NORMAL_SPEED
+            2 -> ActionEnum.STANDING
+            3 -> ActionEnum.SITTING_BENT_BACKWARD
+            4 -> ActionEnum.SITTING_STRAIGHT
+            5 -> ActionEnum.SITTING_BENT_BACKWARD
+            6 -> ActionEnum.LYING_DOWN_ON_THE_RIGHT_SIDE
+            7 -> ActionEnum.LYING_DOWN_ON_THE_LEFT_SIDE
+            8 -> ActionEnum.LYING_DOWN_ON_THE_BACK
+            9 -> ActionEnum.LYING_DOWN_ON_STOMACH
+            10 -> ActionEnum.MOVEMENT
+            11 -> ActionEnum.RUNNING
+            12 -> ActionEnum.ASCENDING_STAIRS
+            13 -> ActionEnum.DESCENDING_STAIRS
+            else -> ActionEnum.LOADING
+        }
     }
 
     private fun setupPage() {
+        lastMovement = ActionEnum.LOADING
         model = Model.newInstance(this)
         classifiedMovementField = findViewById(R.id.movement)
         classifiedMovementField.text = ActionEnum.LYING_DOWN_ON_THE_BACK.movement
     }
 
-    private fun updatePage(movement: String) {
-
-        classifiedMovementField.text = movement
+    private fun updatePage(action: ActionEnum) {
+        runOnUiThread {
+            // Stuff that updates the UI
+            classifiedMovementField.text = action.movement
+        }
     }
 
     override fun onDestroy() {
