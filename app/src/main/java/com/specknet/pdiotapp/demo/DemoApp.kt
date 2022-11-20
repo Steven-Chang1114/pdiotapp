50af3b1b6a879bacd8a026e14200f6ee6d819be2
Steven-Chang1114 <s1992413@ed.ac.uk>
2022-11-20 17:28:32 +0000

    wire up data

--- app/src/main/java/com/specknet/pdiotapp/demo/DemoApp.kt
+++ app/src/main/java/com/specknet/pdiotapp/demo/DemoApp.kt
@@ -4,16 +4,17 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import android.os.Bundle
-import android.os.Handler
-import android.os.HandlerThread
-import android.os.Looper
+import android.os.*
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.github.kittinunf.fuel.core.extensions.jsonBody
+import com.github.kittinunf.fuel.httpPost
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import com.google.android.gms.auth.api.signin.GoogleSignIn
@@ -21,15 +22,23 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.gson.Gson
 import com.specknet.pdiotapp.R
 import com.specknet.pdiotapp.ml.Model
 import com.specknet.pdiotapp.utils.Constants
 import com.specknet.pdiotapp.utils.RESpeckLiveData
 import com.specknet.pdiotapp.utils.ThingyLiveData
+import org.json.JSONArray
 import org.tensorflow.lite.DataType
 import org.tensorflow.lite.Interpreter
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.net.HttpURLConnection
+import java.net.URI
+import java.net.URL
 import java.nio.FloatBuffer
+import java.time.Instant
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 
 class DemoApp : AppCompatActivity() {
 
@@ -37,7 +46,11 @@ class DemoApp : AppCompatActivity() {
     lateinit var dataSet_res_accel_x: LineDataSet
     lateinit var dataSet_res_accel_y: LineDataSet
     lateinit var dataSet_res_accel_z: LineDataSet
+
     lateinit var data : MutableList<FloatArray>
+    lateinit var respeckData : MutableList<List<Float>>
+    lateinit var thingyData : MutableList<List<Float>>
+
     lateinit var model: Model
     lateinit var floatArrayBuffer: FloatBuffer
     lateinit var lastMovement: ActionEnum
@@ -46,21 +59,21 @@ class DemoApp : AppCompatActivity() {
     lateinit var userId : String
     lateinit var db : FirebaseFirestore
 
-    lateinit var respackActiveBtn: Button
+    lateinit var respeckActiveBtn: Button
     lateinit var thingyActiveBtn: Button
     lateinit var cloudActiveBtn: Button
     lateinit var localActiveBtn: Button
     lateinit var actionImage: ImageView
     lateinit var title: TextView
 
-    var isRespackActive = true
+    var isrespeckActive = true
     var isThingyActive = false
     var isCloudActive = true
 
     lateinit var classifiedMovement: ActionEnum
     private lateinit var classifiedMovementField : TextView
 
-    var respackCounter = 0
+    var respeckCounter = 0
     var thingyCounter = 0
     lateinit var allRespeckData: LineData
 
@@ -83,10 +96,17 @@ class DemoApp : AppCompatActivity() {
 
         setupClickListeners()
 
-        onRespackReceive()
+        onrespeckReceive()
 
         onThingyReceive()
 
+    }
+
+    private fun updatePage(action: ActionEnum) {
+        runOnUiThread {
+            // Stuff that updates the UI
+            classifiedMovementField.text = action.movement
+        }
     }
 
     private fun onThingyReceive() {
@@ -109,10 +129,35 @@ class DemoApp : AppCompatActivity() {
                             Toast.LENGTH_SHORT).show()
                     }
 
-                    // get all relevant intent contents
-                    val x = liveData.accelX
-                    val y = liveData.accelY
-                    val z = liveData.accelZ
+                    if (isThingyActive) {
+                        // get all relevant intent contents
+                        val timestamp = System.currentTimeMillis()/1000.toFloat();
+
+                        val accelX = liveData.accelX
+                        val accelY = liveData.accelY
+                        val accelZ = liveData.accelZ
+
+                        val gyroX = liveData.gyro.x
+                        val gyroY = liveData.gyro.y
+                        val gyroZ = liveData.gyro.z
+
+                        val magX = liveData.mag.x
+                        val magY = liveData.mag.y
+                        val magZ = liveData.mag.z
+
+                        val thingyArr = listOf(timestamp, accelX, accelY, accelZ, magX, magY, magZ)
+
+                        while (thingyData.size > 50) {
+                            Log.d("PDIOT_DEMO_CURRENT_THI_SIZE", thingyData.size.toString())
+                            thingyData.removeAt(0)
+                        }
+
+                        thingyData.add(thingyArr)
+
+                        if (thingyData.size >= 50) {
+                            classifiedMovementOnCloud(respeckData.toList(), thingyData.toList())
+                        }
+                    }
 
                     thingyCounter += 1
 
@@ -128,12 +173,12 @@ class DemoApp : AppCompatActivity() {
         this.registerReceiver(thingyLiveUpdateReceiver, filterTestThingy, null, handlerThingy)
     }
 
-    private fun onRespackReceive() {
+    private fun onrespeckReceive() {
         // set up the broadcast receiver
         respeckLiveUpdateReceiver = object : BroadcastReceiver() {
             override fun onReceive(context: Context, intent: Intent) {
 
-                Log.i("Respack_demo_thread", "I am running on thread = " + Thread.currentThread().name)
+                Log.i("respeck_demo_thread", "I am running on thread = " + Thread.currentThread().name)
 
                 val action = intent.action
 
@@ -141,30 +186,48 @@ class DemoApp : AppCompatActivity() {
 
                     val liveData =
                         intent.getSerializableExtra(Constants.RESPECK_LIVE_DATA) as RESpeckLiveData
-                    Log.d("Respack_demo_Live", "onReceive: liveData = " + liveData)
+                    Log.d("Respeck_demo_Live", "onReceive: liveData = " + liveData)
 
-                    if (respackCounter == 0) {
-                        Toast.makeText(baseContext, "Respack is running",
+                    if (respeckCounter == 0) {
+                        Toast.makeText(baseContext, "Respeck is running",
                             Toast.LENGTH_SHORT).show()
                     }
 
-                    // get all relevant intent contents
-                    val accelX = liveData.accelX
-                    val accelY = liveData.accelY
-                    val accelZ = liveData.accelZ
+                    if (isrespeckActive) {
+                        // get all relevant intent contents
+                        val timestamp = System.currentTimeMillis()/1000.toFloat();
 
-                    val x = liveData.gyro.x
-                    val y = liveData.gyro.y
-                    val z = liveData.gyro.z
+                        val accelX = liveData.accelX
+                        val accelY = liveData.accelY
+                        val accelZ = liveData.accelZ
 
-//                    data.add(floatArrayOf(accelX, accelY, accelZ, x, y, z))
+                        val gyroX = liveData.gyro.x
+                        val gyroY = liveData.gyro.y
+                        val gyroZ = liveData.gyro.z
+
+                        val resultArr = listOf(timestamp, accelX, accelY, accelZ, gyroX, gyroY, gyroZ)
+
+                        while (respeckData.size > 50) {
+                            Log.d("PDIOT_DEMO_CURRENT_RES_SIZE", respeckData.size.toString())
+                            respeckData.removeAt(0)
+                        }
+
+                        respeckData.add(resultArr)
+
+
+                        if (respeckData.size >= 50) {
+                            classifiedMovementOnCloud(respeckData.toList(), thingyData.toList())
+                        }
+
+                        //                    data.add(floatArrayOf(accelX, accelY, accelZ, x, y, z))
 //                    floatArrayBuffer.put(floatArrayOf(accelX, accelY, accelZ, x, y, z))
 
-                    // Send data to the ML model and run for update
+                        // Send data to the ML model and run for update
 //                    classifyMovement(floatArrayBuffer)
+                        // updateGraph("respeck", x, y, z)
+                    }
 
-                    respackCounter += 1
-                    // updateGraph("respeck", x, y, z)
+                    respeckCounter += 1
 
                 }
             }
@@ -178,19 +241,51 @@ class DemoApp : AppCompatActivity() {
         this.registerReceiver(respeckLiveUpdateReceiver, filterTestRespeck, null, handlerRespeck)
     }
 
-    private fun setupClickListeners() {
-        respackActiveBtn.setOnClickListener {
-            if (isRespackActive) {
-                isRespackActive = false
-                respackActiveBtn.setBackgroundResource(R.drawable.hardware_button_inactive)
-            } else {
-                isRespackActive = true
-                respackActiveBtn.setBackgroundResource(R.drawable.hardware_button_active)
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun classifiedMovementOnCloud(respeckData : List<List<Float>>, thingyData: List<List<Float>>) {
+        if (isrespeckActive && isThingyActive) {
+            if (respeckData.size >= 50 && thingyData.size >= 50) {
+
+
+                val values = Gson().toJson(mapOf("type" to "both", "thingy_json" to thingyData.toString(), "respeck_json" to respeckData.toString()))
+
+//                saveData(values)
+                Log.d("PDIOT_DEMO_INPUT", listOf("json" to values).toString())
+
+                val (_, _, result) = "https://pdiot.azurewebsites.net/api/inferencehttptrigger"
+                    .httpPost(listOf("json" to values))
+                    .responseString()
+
+                Log.d("PDIOT_DEMO_RESULT", result.toString())
+
             }
+        } else if (isrespeckActive) {
+            if (respeckData.size >= 50) {
+
+            }
+
+        } else if (isThingyActive) {
+            if (thingyData.size >= 50) {
+
+            }
+        }
+    }
+
+    private fun setupClickListeners() {
+        respeckActiveBtn.setOnClickListener {
+            if (isrespeckActive) {
+                isrespeckActive = false
+                respeckActiveBtn.setBackgroundResource(R.drawable.hardware_button_inactive)
+            } else {
+                isrespeckActive = true
+                respeckActiveBtn.setBackgroundResource(R.drawable.hardware_button_active)
+            }
+
+            respeckCounter = 0
+            respeckData = mutableListOf<List<Float>>()
         }
 
         thingyActiveBtn.setOnClickListener {
-
             if (isThingyActive) {
                 isThingyActive = false
                 thingyActiveBtn.setBackgroundResource(R.drawable.hardware_button_inactive)
@@ -199,6 +294,8 @@ class DemoApp : AppCompatActivity() {
                 thingyActiveBtn.setBackgroundResource(R.drawable.hardware_button_active)
             }
 
+            thingyCounter = 0
+            thingyData = mutableListOf<List<Float>>()
         }
 
         cloudActiveBtn.setOnClickListener {
@@ -273,10 +370,12 @@ class DemoApp : AppCompatActivity() {
     private fun setupPage() {
         lastMovement = ActionEnum.GENERAL_MOVEMENT
         model = Model.newInstance(this)
-
         db = Firebase.firestore
 
-        respackActiveBtn = findViewById(R.id.respack_button)
+        respeckData = mutableListOf<List<Float>>()
+        thingyData = mutableListOf<List<Float>>()
+
+        respeckActiveBtn = findViewById(R.id.respeck_button)
         thingyActiveBtn = findViewById(R.id.thingy_button)
         cloudActiveBtn = findViewById(R.id.azure_button)
         localActiveBtn = findViewById(R.id.local_button)
@@ -286,39 +385,44 @@ class DemoApp : AppCompatActivity() {
 
         getUserId()
 
-//        val user = hashMapOf(
-//            "first" to "DBDBD",
-//            "middle" to "AAAA",
-//            "last" to "sfsefse",
-//            "born" to 300
-//        )
-//
-//        // Add a new document with a generated ID
-//        db.collection(userId)
-//            .add(user)
-//            .addOnSuccessListener { documentReference ->
-//                Log.d("DB_TEST", "DocumentSnapshot added with ID: ${documentReference.id}")
-//            }
-//            .addOnFailureListener { e ->
-//                Log.w("DB_TEST", "Error adding document", e)
-//            }
-
-//        db.collection(userId)
-//            .get()
-//            .addOnSuccessListener { result ->
-//                for (document in result) {
-//                    Log.d(TAG, "${document.id} => ${document.data}")
-//                }
-//            }
-//            .addOnFailureListener { exception ->
-//                Log.w(TAG, "Error getting documents.", exception)
-//            }
-
         val username = intent.getStringExtra("name")
         title.text = String.format("%s's\ncurrent action:", username)
 
         classifiedMovementField.text = ActionEnum.LYING_DOWN_ON_THE_RIGHT_SIDE.movement
         actionImage.setBackgroundResource(R.drawable.general_movement)
+    }
+
+    private fun readData() {
+        db.collection(userId)
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d("PDIOT_DB", "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("PDIOT_DB", "Error getting documents.", exception)
+            }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun saveData(data: String) {
+        val curTimestamp = DateTimeFormatter
+            .ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS")
+            .withZone(ZoneOffset.UTC)
+            .format(Instant.now())
+
+        // Add a new document with a generated ID
+        db.collection(userId)
+            .add(hashMapOf(
+                curTimestamp to data
+            ))
+            .addOnSuccessListener { documentReference ->
+                Log.d("PDIOT_DB", "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w("PDIOT_DB", "Error adding document", e)
+            }
     }
 
     private fun getUserId() {
@@ -335,18 +439,16 @@ class DemoApp : AppCompatActivity() {
         Log.d("PDIOT_FIREBASE_DEMO", userId)
     }
 
-    private fun updatePage(action: ActionEnum) {
-        runOnUiThread {
-            // Stuff that updates the UI
-            classifiedMovementField.text = action.movement
-        }
-    }
-
     override fun onDestroy() {
         super.onDestroy()
         unregisterReceiver(respeckLiveUpdateReceiver)
         unregisterReceiver(thingyLiveUpdateReceiver)
+
+        respeckData = mutableListOf<List<Float>>()
+        thingyData = mutableListOf<List<Float>>()
+
         model.close()
+
         looperRespeck.quit()
         looperThingy.quit()
     }
